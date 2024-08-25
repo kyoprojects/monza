d21127ee4a6e826924fd0f7e1d95085e45314f68
sleakchat <support@sleak.chat>
2024-08-25 15:44:56 +0200

    disable lenis

--- monza.js
+++ monza.js
@@ -180,13 +180,13 @@ preloadImages().then(() => {
   });
 });
 
-// lenis
-const lenis = new Lenis();
-function raf(time) {
-  lenis.raf(time);
-  requestAnimationFrame(raf);
-}
-requestAnimationFrame(raf);
+// // lenis
+// const lenis = new Lenis();
+// function raf(time) {
+//   lenis.raf(time);
+//   requestAnimationFrame(raf);
+// }
+// requestAnimationFrame(raf);
 
 // circle text
 new CircleType(document.getElementById('circletext'));
