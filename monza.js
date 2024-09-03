a74dacaf210d955e20a0b9e10b870e2c0896a2a6
sleakchat <support@sleak.chat>
2024-09-03 19:42:33 +0200

    refactor: Adjust hero slider animation for better performance

--- monza.js
+++ monza.js
@@ -159,21 +159,21 @@ mm.add('(max-width: 400px)', () => {
     .timeline({
       scrollTrigger: { trigger: '.image-slide-sectionwrap', start: 'top bottom', end: 'bottom top', scrub: 3, markers: false, toggleActions: 'play none none none' }
     })
-    .fromTo('[image-slide-1]', { y: '100px' }, { y: '-100px' })
-    .fromTo('[image-slide-2]', { y: '200px' }, { y: '-150px' }, '<')
+    .fromTo('[image-slide-1]', { y: '100px' }, { y: '-150px' })
+    .fromTo('[image-slide-2]', { y: '200px' }, { y: '-200px' }, '<')
     .fromTo('[image-slide-3]', { y: '300px' }, { y: '-100px' }, '<')
-    .fromTo('[image-slide-4]', { y: '200px' }, { y: '-300px' }, '<')
-    .fromTo('[image-slide-5]', { y: '400px' }, { y: '0px' }, '<');
+    .fromTo('[image-slide-4]', { y: '500px' }, { y: '-200px' }, '<')
+    .fromTo('[image-slide-5]', { y: '300px' }, { y: '-150px' }, '<');
 });
 mm.add('(min-width: 401px)', () => {
   gsap.timeline({
     scrollTrigger: { trigger: '.image-slide-sectionwrap', start: 'top bottom', end: 'bottom top', scrub: 3, markers: false, toggleActions: 'play none none none' }
   });
-  // .fromTo('[image-slide-1]', { y: '100px' }, { y: '-100px' })
-  // .fromTo('[image-slide-2]', { y: '500px' }, { y: '-350px' }, '<')
-  // .fromTo('[image-slide-3]', { y: '600px' }, { y: '-200px' }, '<')
-  // .fromTo('[image-slide-4]', { y: '600px' }, { y: '-200px' }, '<')
-  // .fromTo('[image-slide-5]', { y: '850px' }, { y: '-550px' }, '<');
+  .fromTo('[image-slide-1]', { y: '100px' }, { y: '-100px' })
+  .fromTo('[image-slide-2]', { y: '500px' }, { y: '-350px' }, '<')
+  .fromTo('[image-slide-3]', { y: '600px' }, { y: '-200px' }, '<')
+  .fromTo('[image-slide-4]', { y: '600px' }, { y: '-200px' }, '<')
+  .fromTo('[image-slide-5]', { y: '850px' }, { y: '-550px' }, '<');
 
   /// threejs distorted images on hover
   const container = document.querySelector('.triggercontainer');
